2bdd78084befe36322fa65f5264ee399f1fec948
nick-sohl <nicksohl59@gmail.com>
2025-11-06 18:30:33 +0100

    add server config for live update inside docker container

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
 export default defineConfig({
+  vite: {
     plugins: [tailwindcss()],
+    server: {
+      host: "0.0.0.0",
+      port: 4321,
+      watch: { usePolling: true }
     }
-  }, 
+  },
 });
